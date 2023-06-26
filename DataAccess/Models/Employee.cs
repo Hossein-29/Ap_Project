82a942e303305e389e0b5f01318fb4437075183a
hossein <hb3238816@gmail.com>
2023-06-26 13:58:35 -0700

    initial project

---
+++ DataAccess/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Employee : IPerson
+    {
+        public int id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string firstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string lastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string email { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string userName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string password { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ DataAccess/Models/IPerson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public interface IPerson
+    {
+        int id { get; set; }
+        string firstName { get; set; }
+        string lastName { get; set; }
+        string email { get; set; }
+        string userName { get; set; }
+        string password { get; set; }
+    }
+}
