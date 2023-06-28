8a978945aa9465f9b0d4568afee9317451fcc44f
Abolfazl Shahidi <shahidi2004shahidi@gmail.com>
2023-06-28 16:58:13 +0330

    Complete OrderRegistering Sention (Employee Panel)

--- WpfPostCompany/MainOrderRegistrationWindow.xaml.cs
+++ WpfPostCompany/MainOrderRegistrationWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,52 @@ namespace WpfPostCompany
         {
             InitializeComponent();
         }
+        private double FinalPrice()
+        {
+            double Price = 10000;
+            double Weight = double.Parse(PackageWeight.Text.ToString());
+            if (PackageType.SelectedIndex == 1)
+                Price *= 1.5;
+            else if (PackageType.SelectedIndex == 2)
+                Price *= 2;
 
+            if (PostType.SelectedIndex == 1)
+                Price *= 1.5;
+
+            if (HasExpensiveContent.IsChecked == true)
+                Price *= 2;
+
+            if (Weight > 0.5)
+            {
+                Weight -= 0.5;
+                int Coefficient = 0;
+                Coefficient += (int)Math.Ceiling(Weight / 0.5);
+                if(Coefficient != 0)
+                Price *= Coefficient * 1.2;
+            }
+            return Price;
+        }
+
+        private void CalculateFinalPrice(object sender, RoutedEventArgs e)
+        {
+            if (SenderAddress.Text == "")
+                MessageBox.Show("please enter sender address");
+            else if (RecieverAddress.Text == "")
+                MessageBox.Show("please enter reciever address");
+            else if (PackageType.SelectedItem == null)
+                MessageBox.Show("please select type of package");
+            else if (PostType.SelectedItem == null)
+                MessageBox.Show("please select type of post");
+            else if (PackageWeight.Text == "")
+                MessageBox.Show("please enter weight");
+            else if (double.IsNaN(double.Parse(PackageWeight.Text.ToString())))
+                MessageBox.Show("invalid weight value");
+            else if (PhoneNumber.Text != "" && !InputValidation.PhoneValidation(PhoneNumber.Text.ToString()))
+                MessageBox.Show("invalid phone number");
+            else
+            {
+                MessageBox.Show($"Final Price : {FinalPrice()}", "", MessageBoxButton.OKCancel);
+            }
+        }
     }
 }
