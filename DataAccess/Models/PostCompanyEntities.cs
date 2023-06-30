e24233c83029370fe749c689a82c75e9c4e09335
Abolfazl Shahidi <shahidi2004shahidi@gmail.com>
2023-06-30 17:13:38 +0330

    Creating Model Again!!

--- DataAccess/Models/PostCompanyEntities.cs
+++ DataAccess/Models/PostCompanyEntities.cs
@@ -1,1 +1,74 @@
-﻿
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DataAccess.Models
+{
+    public partial class PostCompanyEntities : DbContext
+    {
+        public PostCompanyEntities()
+            : base("name=PostCompanyEntitiesDB")
+        {
+        }
+
+        public virtual DbSet<Costumer> Costumer { get; set; }
+        public virtual DbSet<Employee> Employee { get; set; }
+        public virtual DbSet<Order> Order { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Costumer>()
+                .Property(e => e.FirstName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Costumer>()
+                .Property(e => e.LastName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Costumer>()
+                .Property(e => e.SSN)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Costumer>()
+                .Property(e => e.Phone)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Costumer>()
+                .Property(e => e.UserName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Costumer>()
+                .Property(e => e.Password)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.FirstName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.LastName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.PersonalID)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.UserName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Password)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.Phone)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.CostumerSSN)
+                .IsUnicode(false);
+        }
+    }
+}
